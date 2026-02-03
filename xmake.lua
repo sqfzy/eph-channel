35dde55e72fb1c8401d9d5dca8e0260272831efb
sqfzy <2199368711@qq.com>
2026-02-03 09:59:22 +0800

    update

--- xmake.lua
+++ xmake.lua
@@ -1,30 +1,8 @@
-package("ephemeral")
-    -- 1. 基础元数据
-    set_description("High-frequency trading C++ primitive")
-    set_urls("https://github.com/sqfzy/ephemeral.git")
-    add_versions("1.0", "87b112a")
-
-    -- 2. 安装逻辑
-    on_install(function (package)
-        import("package.tools.xmake").install(package)
-    end)
-
-    -- 3. 测试
-    on_test(function (package)
-        package:check_cxxsnippets({test = [[
-            #include <eph/platform.hpp>
-            void test() {
-                eph::cpu_relax();
-            }
-        ]]}, {configs = {languages = "c++23"}})
-    end)
-
-
 set_project("eph")
 set_version("1.0.0")
 
-add_rules("mode.debug", "mode.release")
 
+add_rules("mode.debug", "mode.release")
 set_languages("c++23")
 
 if is_mode("release") then
@@ -103,3 +81,28 @@ for _, file in ipairs(os.files("tests/**/*.cpp")) do
     set_default(false)
     add_tests("default") -- 允许 xmake test 运行
 end
+
+
+package("eph")
+    set_kind("library", {headeronly = true})
+    set_description("High-frequency trading C++ primitive")
+
+    set_urls("https://github.com/sqfzy/ephemeral.git")
+    add_versions("1.0", "77de1d2")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        package:check_cxxsnippets({test = [[
+            #include <eph/platform.hpp>
+            void test() {
+                eph::cpu_relax();
+            }
+        ]]}, {configs = {languages = "c++23"}})
+    end)
